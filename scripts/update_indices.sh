eebd50b86c5f41958c46c65fbb2f9c23e6ae6b50
dobretsindriver <gennadiy.dobretsov@indriver.com>
2022-06-08 01:46:33 +0600

    stop service

--- scripts/update_indices.sh
+++ scripts/update_indices.sh
@@ -1,9 +1,12 @@
 # Обновление индексов на новое поколение
-ssh root@95.217.236.128 "cp dgs/dg_2/ indices/ -r"
-ssh root@95.217.239.216 "cp dgs/dg_2/ indices/ -r"
+ssh root@95.217.236.128 "cp /root/hardml_mlops_final/dgs/dg_2/ indices/ -r"
+ssh root@95.217.239.216 "cp /root/hardml_mlops_final/dgs/dg_2/ indices/ -r"
 
 # rolling update сервиса индекса кластера
-ssh root@95.217.236.128 "docker service update --force --update-parallelism 1 --update-delay 30s qa_index_service_$3"
+ssh root@95.217.236.128 "docker service update --force --update-parallelism 1 --update-delay 30s qa_index_service_0"
+ssh root@95.217.236.128 "docker service update --force --update-parallelism 1 --update-delay 30s qa_index_service_1"
+ssh root@95.217.236.128 "docker service update --force --update-parallelism 1 --update-delay 30s qa_index_service_2"
+ssh root@95.217.236.128 "docker service update --force --update-parallelism 1 --update-delay 30s qa_index_service_3"
 
 # rolling update сервиса gateway
 ssh root@95.217.236.128 "docker service update --force --update-parallelism 1 --update-delay 30s qa_gateway"
