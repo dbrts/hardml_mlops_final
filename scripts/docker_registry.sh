77ddbbf1109d2f148e47f18b31c0a51d25b9ff46
dobretsindriver <gennadiy.dobretsov@indriver.com>
2022-06-08 01:21:21 +0600

    uncomment registry

--- scripts/docker_registry.sh
+++ scripts/docker_registry.sh
@@ -1,6 +1,6 @@
 # если уже запущен не надо
 # создание регистра
-#docker run -d -p 5000:5000 --restart=always --name registry registry:2
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 # загрузка gateway
 docker tag gateway 95.217.236.128:5000/gateway
