41e0650281a6a3e996fd49f8ce3aa8f4bf125e18
dobretsindriver <gennadiy.dobretsov@indriver.com>
2022-06-08 01:04:57 +0600

    tf_random registry

--- scripts/docker_registry.sh
+++ scripts/docker_registry.sh
@@ -1,5 +1,6 @@
+# если уже запущен не надо
 # создание регистра
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
+#docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 # загрузка gateway
 docker tag gateway 95.217.236.128:5000/gateway
@@ -8,3 +9,7 @@ docker push 95.217.236.128:5000/gateway
 # загрузка index_service
 docker tag index_service 95.217.236.128:5000/index_service
 docker push 95.217.236.128:5000/index_service
+
+# загрузка tf_random
+docker tag tf_random 95.217.236.128:5000/tf_random
+docker push 95.217.236.128:5000/tf_random
